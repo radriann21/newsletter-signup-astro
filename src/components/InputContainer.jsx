b636da4fe2fd78745bacb777052169fafdc40723
radriann21 <adrianrangelargu@gmail.com>
2024-07-26 19:17:48 -0400

    finished project

--- src/components/InputContainer.jsx
+++ src/components/InputContainer.jsx
@@ -4,11 +4,35 @@ import { SubscribedMessage } from "./SuscribedMessage"
 
 export const InputContainer = () => {
 
-  const [valid, setValid] = useState(true)
+  const [valid, setValid] = useState(null)
+  const [email, setEmail] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+  
+  const handleEmailValue = (e) => {
+    const newEmail = e.target.value
+    setEmail(newEmail)
+
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    setValid(regex.test(newEmail))
+  }
+
+  const validEmail = (e) => {
+    e.preventDefault()
+    if (valid) {
+      setValid(true)
+      setSubmitted(true)
+    } else {
+      setValid(false)
+    }
+  }
 
   return (
     <article className="w-full h-full rounded-none lg:w-fit lg:h-fit bg-white shadow-lg md:rounded-lg flex items-center justify-center p-0 lg:p-6">
-      {valid ? (<SubscribedMessage />) : (<NewsletterForm />)}
+      {
+      submitted 
+        ? (<SubscribedMessage email={email} />) 
+        : (<NewsletterForm handleEmailValue={handleEmailValue} validEmail={validEmail} email={email} valid={valid} />)
+      }
     </article>
   )
 }
--- src/components/NewsletterForm.jsx
+++ src/components/NewsletterForm.jsx
@@ -1,15 +1,38 @@
 import { IllustrationSection } from "./IllustrationSection.jsx"
-import { FormComponent } from "./FormComponent.jsx"
 import { TextComponent } from "./TextComponent.jsx"
 
-export const NewsletterForm = () => {
+export const NewsletterForm = ({ handleEmailValue, email, validEmail, valid }) => {
 	return (
 		<div className="w-full h-full flex justify-between items-center flex-col lg:flex-row-reverse">
 			<IllustrationSection />
 			<section className="w-full h-full basis-[60%] mt-8 px-4 lg:py-8 lg:px-4">
 				<TextComponent />
-				<FormComponent />
+
+				<section className="mt-8">
+					<form onSubmit={validEmail}>
+						<label className="flex w-full justify-between" htmlFor="email">
+							<span className="text-sm font-global font-bold">Email address</span>
+							<span className={`${valid === false ? '' : 'hidden'} text-sm font-global font-bold text-red-600`}>
+								Valid email required
+							</span>
+						</label>
+						<input 
+							onChange={handleEmailValue} 
+							className={`border-2 ${valid === false ? 'border-red-600' : ''} p-3 w-full rounded-md outline-none ${valid === false ? 'focus:border-red-600' : 'focus:border-dark-slate-grey'}`} 
+							type="email" 
+							name="email" 
+							placeholder="email@company.com" 
+							value={email} 
+							autoComplete="off" 
+						/>
+						<button
+							className="text-white text-sm w-full rounded-md p-3 mt-4 font-global font-bold bg-dark-slate-grey text-center transition-colors duration-300 ease-in hover:bg-charcoal-grey">
+							Subscribe to monthly newsletter
+						</button>
+					</form>
+				</section>
 			</section>
+
 		</div>
 	)
 }
